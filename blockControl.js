68edb86f97409c82908485019d2e168d2d03e143
Caroline Kaleel <cocokaleel@gmail.com>
2019-10-30 19:45:14 -0700

    Adding block control
    it is now possible to place blocks!

--- blockControl.js
+++ blockControl.js
@@ -1,7 +1,57 @@
-blocks[];
-drawBlock() {
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var brickHeight = 10;
+var brickWidth = 75;
+var mouseX = 0;
+var mouseY = 0;
 
+document.addEventListener("mousemove", mouseMoveHandler, false);
+document.addEventListener('mousedown', e => {
+  placeBrick();
+});
+
+//purpose: deals with the moving mouse
+//keeps mouseX and mouseY as the coordinates of the cursor
+function mouseMoveHandler(e) {
+  var relativeX = e.clientX - canvas.offsetLeft;
+  var relativeY = e.clientY - canvas.offsetTop;
+  if(relativeX > 0 && relativeX < canvas.width) {
+    mouseX = relativeX;
+  }
+  if(relativeY > 0 && relativeY < canvas.height) {
+    mouseY = relativeY;
+  }
 }
-placeBlock (x,y) {
 
+//purpose: draws the floating brick around the cursor
+function drawStamp() {
+  if (bricksLeft>0) {
+    ctx.beginPath();
+    ctx.rect(mouseX, mouseY, brickWidth, brickHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+  }
+}
+
+//purpose: draws the array of bricks (bricks[]) onto the canvas
+function drawBricks() {
+  ctx.beginPath();
+  for (i=0; i<bricks.length; i++) {
+    ctx.rect(bricks[i].x, bricks[i].y, brickWidth, brickHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+  }
+  ctx.closePath();
+}
+
+//purpose: adds brick when mousepad is clicked
+//subtracts one from number of bricks left to place
+//pushes new brick onto brick[] array with coordinates at current mouse location
+function placeBrick() {
+  if (bricksLeft>0) {
+    var point= {x:mouseX,y:mouseY};
+    bricks.push(point);
+    bricksLeft--;
+  }
 }
---
+++ main.js
@@ -0,0 +1,25 @@
+var score = 0;
+var level = 1;
+var bricksLeft = 2;
+var bricks = [];
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBricks();
+    drawStamp();
+    drawStage();
+}
+
+function drawStage() {
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "#0095DD";
+    ctx.fillText("Score: "+score, 8, 20);
+    if (bricksLeft>0) {
+        ctx.fillText("Bricks left to place: "+bricksLeft,8,40);
+    }
+    else {
+        ctx.fillText("Click spacebar to start.", 8,40);
+    }
+}
+
+var interval = setInterval(draw, 10);
